919c6cab40a57ecad6c145de7508864a8bb883a1
betterwebsite <normanwebdesigners@gmail.com>
2025-06-10 16:40:17 -0500

    more updates
    Jordan Babb

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,21 +1,21 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "Riddick Big Daddy Bowe Boxing",
+    "email": "info@RiddickBigDaddyBoweBoxing.com",
+    "phoneForTel": "941-500-25477",
+    "phoneFormatted": "(941) 500-2547",
     "address": {
         "lineOne": "First Address Line",
         "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
+        "city": "",
+        "state": "FL",
+        "zip": "",
         "country": "US",
         "mapLink": "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6"
     },
     "socials": {
-        "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
+        "facebook": "https://www.facebook.com/profile.php?id=61551650260629",
+        "instagram": "https://www.instagram.com/riddickbigdaddyboweboxingllc/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://www.riddickbigdaddyboweboxing.com"
 };
